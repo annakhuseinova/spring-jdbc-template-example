5389497e84828beb3824c8b3abb8e6fa72c1e454
Anna Khuseinova <annettekhuseinova@gmail.com>
2024-02-09 23:22:33 +0300

    Complemented properties

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO course(title, description, link) values ('Course1','Description1','https://www.udemy.com/course/1');
-INSERT INTO course(title, description, link) values ('Course2','Description2','https://www.udemy.com/course/2');
-INSERT INTO course(title, description, link) values ('Course3','Description3','https://www.udemy.com/course/3');
-INSERT INTO course(title, description, link) values ('Course4','Description4','https://www.udemy.com/course/4');
-INSERT INTO course(title, description, link) values ('Course5','Description5','https://www.udemy.com/course/5');
+INSERT INTO course(course_id, title, description, link) values (1, 'Course1','Description1','https://www.udemy.com/course/1');
+INSERT INTO course(course_id, title, description, link) values (2, 'Course2','Description2','https://www.udemy.com/course/2');
+INSERT INTO course(course_id, title, description, link) values (3, 'Course3','Description3','https://www.udemy.com/course/3');
+INSERT INTO course(course_id, title, description, link) values (4, 'Course4','Description4','https://www.udemy.com/course/4');
+INSERT INTO course(course_id, title, description, link) values (5, 'Course5','Description5','https://www.udemy.com/course/5');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,7 @@
 CREATE TABLE course (
-
-    course_id integer identity NOT NULL,
-    title varchar(88) NOT NULL,
-    description varchar(580) NOT NULL,
-    link varchar(255) NOT NULL,
+    course_id integer NOT NULL,
+    title varchar(80) NOT NULL,         -- Course Title
+    description varchar(500) NOT NULL,  -- Course Description
+    link varchar(255) NOT NULL,         -- Course landing page link
     CONSTRAINT pk_course_course_id PRIMARY KEY (course_id)
-
 );
